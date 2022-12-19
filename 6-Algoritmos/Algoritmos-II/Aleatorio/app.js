e90d796c8a00f1a8d1aa79c0691d43d6cfebd517
claraortiz <clara.ortizdelatorre@gmail.com>
2022-12-19 19:17:45 +0100

    aleatorio

---
+++ 6-Algoritmos/Algoritmos-II/Aleatorio/app.js
@@ -0,0 +1,9 @@
+var randomPick = (n, min, max) => {
+    for(let i=0 ; i < n ; i++){
+        console.log(Math.round(Math.random() * (max - min) + min));
+    }
+  }
+
+
+
+  randomPick(10, 1, 100);
