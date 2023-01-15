1b1dfd39e9728f767fac250d71ce3e88f0acdc62
claraortiz <clara.ortizdelatorre@gmail.com>
2023-01-15 21:40:44 +0100

    :sparkles: corregido aleatorio

--- 6-Algoritmos/Algoritmos-II/Aleatorio/app.js
+++ 6-Algoritmos/Algoritmos-II/Aleatorio/app.js
@@ -1,7 +1,27 @@
 var randomPick = (n, min, max) => {
-    for(let i=0 ; i < n ; i++){
-        console.log(Math.round(Math.random() * (max - min) + min));
-    }
+    let number = []
+    let number_ale = 0;
+    let index = 0;
+
+    do{
+        number_ale = Math.round(Math.random() * (max - min) + min);
+        if(number.length === 0){
+            number.push(number_ale)
+            index++;
+        }else if(!number.includes(number_ale)){
+            number.push(number_ale);
+            index++;
+        }
+
+    }while(index < n);
+
+    
+    
+    console.log(number);
   }
 
-  randomPick(10, 1, 100);
+
+  randomPick(6, 1, 6);
+
+  //meterlos en array y print, no se pueden repetir numeros
+  //2 funciones, no bucle for , 6 numeros , del 1 al 6
