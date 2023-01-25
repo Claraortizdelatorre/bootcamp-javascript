e1f3ff36d2789b4242c9bf29711b92dff5d56b9c
claraortiz <clara.ortizdelatorre@gmail.com>
2023-01-25 14:00:23 +0100

    :sparkles: modificaciones carrito compra

--- 5-Arrays/carrito-compra/js/app_html.js
+++ 5-Arrays/carrito-compra/js/app_html.js
@@ -4,41 +4,38 @@ let div =  document.getElementById("app");
 let nameProduct = "";
 let priceProduct = "";
 let countProduct = "";
-let buttonProduct = "";
+let buttonProduct = document.getElementById("button-delete");
 
-    getCarrito().forEach(function(article) {
-        console.log(article.name)
+var array = getCarrito();
+    for(let i = 0 ; i <= array.length ; i++){
+
         let divOneProduct = document.createElement("div");
         let ulProduct = document.createElement("ul");
         nameProduct = document.createElement("p");
-        nameProduct.innerHTML = "Producto: " + article.name;
+        nameProduct.innerHTML = "Producto: " + array[i].name;
         priceProduct = document.createElement("p");
-        priceProduct.innerHTML = "Precio: " + article.price + "€/ud";
+        priceProduct.innerHTML = "Precio: " + array[i].price + "€/ud";
         countProduct = document.createElement("p");
-        countProduct.innerHTML = "Cantidad: " + article.count;
+        countProduct.innerHTML = "Cantidad: " + array[i].count;
 
         //boton
         buttonProduct = document.createElement("button");
-        buttonProduct.setAttribute('onclick', borrar(divOneProduct));
         buttonProduct.innerHTML = "Eliminar";
+       
 
-
-        //form.appendChild(divProduct);
         div.appendChild(divOneProduct);
         divOneProduct.appendChild(ulProduct);
         ulProduct.appendChild(nameProduct);
         ulProduct.appendChild(priceProduct);
         ulProduct.appendChild(countProduct);
         ulProduct.appendChild(buttonProduct);
+       
 
+        buttonProduct.onclick = function click() {
+             divOneProduct.remove();
+        };
+    }
 
-});
-
-function borrar(divOneProduct) {
-
-    div.removeChild(divOneProduct);
-  
-   };
 
 
 
